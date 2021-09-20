9a5085ebb53dc21f4fa913ae85abd9366b1f1aaa
limseongkyu <96x60812@naver.com>
2021-09-20 17:14:24 +0900

    [Add] getMethod

---
+++ src/main/java/com/loopy/controller/GetController.java
@@ -0,0 +1,18 @@
+package com.loopy.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path ="/getMethod")
+    public String getRequest(){
+
+        return "Hi getMethod";
+
+    }
+}
