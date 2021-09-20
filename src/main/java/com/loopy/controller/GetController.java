67b430710ca1b073b346e01178bd20739a437657
limseongkyu <96x60812@naver.com>
2021-09-20 17:44:09 +0900

    [Add] json형태로 반환

--- src/main/java/com/loopy/controller/GetController.java
+++ src/main/java/com/loopy/controller/GetController.java
@@ -23,15 +23,16 @@ public class GetController {
              return id+password;
     }
     //많은 개수의 변수를 한번에 받는 방법.
-    //getter,setter ?를 이용해서 받는다.
+    //getter,setter(생성자?)를 이용해서 받는다.
     @GetMapping("getMultiParam")
-    public String multiParameter(SearchParam searchParam){
+    //객체를 리턴하게되면 스프링부트가 jackson이라는 내장 라이브러리 이용하여 json 형태로 자동으로 반환.
+    public SearchParam multiParameter(SearchParam searchParam){
         //console에 출력.
         System.out.println(searchParam.getAccount());
         System.out.println(searchParam.getEmail());
         System.out.println(searchParam.getPage());
 
-        return"OK";
+        return searchParam;
     }
 
 }
