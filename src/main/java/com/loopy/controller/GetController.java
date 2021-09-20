72084c8a1a38edeeea12fbfb67161910750b889d
limseongkyu <96x60812@naver.com>
2021-09-20 17:19:49 +0900

    [Add] @RequestParam pactice

--- src/main/java/com/loopy/controller/GetController.java
+++ src/main/java/com/loopy/controller/GetController.java
@@ -1,18 +1,23 @@
 package com.loopy.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class GetController {
 
-    @RequestMapping(method = RequestMethod.GET, path ="/getMethod")
+    @RequestMapping(method = RequestMethod.GET, path ="/getMethod") // /api/getMethod
     public String getRequest(){
 
         return "Hi getMethod";
 
+    }
+    @GetMapping("/getParameter") //localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam String password){
+             System.out.println("id : "+ id);
+             System.out.println("password : "+password);
+
+             return id+password;
     }
 }
