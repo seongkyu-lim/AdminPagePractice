719bb4a67bef78d2f9a8641daa5b551880946604
limseongkyu <96x60812@naver.com>
2021-10-06 13:49:16 +0900

    [Add] Test with Talent api tester

--- src/main/java/com/loopy/model/SearchParam.java
+++ src/main/java/com/loopy/model/SearchParam.java
@@ -1,11 +1,22 @@
 package com.loopy.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+//모든 매개변수를 가지는 생성자를 추가해줌.
+@AllArgsConstructor
 public class SearchParam {
 
     private String account;
     private String email;
     private int page;
 
+
+
+
+
+    /*
     //Code->generates->getter and setter 로 자동으로 생성합니다.
     public String getAccount() {
         return account;
@@ -24,4 +35,6 @@ public class SearchParam {
     public void setPage(int page) {
         this.page = page;
     }
+
+     */
 }
