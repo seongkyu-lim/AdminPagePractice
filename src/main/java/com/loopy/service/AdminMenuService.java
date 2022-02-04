c4e37bed33c438c7861439257a3134504827378b
seongkyu-lim <96x60812@naver.com>
2022-02-04 16:08:26 +0900

    add : thymeleaf

---
+++ src/main/java/com/loopy/service/AdminMenuService.java
@@ -0,0 +1,20 @@
+package com.loopy.service;
+
+import com.loopy.domain.front.AdminMenu;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AdminMenuService {
+
+    public List<AdminMenu> getAdminMenu(){
+
+        return Arrays.asList(
+                AdminMenu.builder().title("고객 관리").url("/pages/user").code("user").build()
+        );
+
+    }
+
+}
